65d6b5a9e251caa6e044e6da243e5733006c0817
Keeghan McGarry <keeghan_m@live.com>
2025-08-25 14:26:03 +0100

    refactor(auth): Improved performance on non-protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,29 @@ import { auth } from '@auth/auth'
 import { defineMiddleware } from 'astro:middleware'
 
 const PROTECTED_ROUTES = ['/dashboard']
+const AUTH_ROUTES = ['/auth']
 
 export const onRequest = defineMiddleware(async (context, next) => {
+  // We check for a non-protected route first, allowing us to skip
+  // unnecessary auth checks on public routes, saving DB queries.
+  // Because we redirect logged-in users away from auth routes, we
+  // want to handle them later
+  if (
+    !PROTECTED_ROUTES.some((route) => context.url.pathname.startsWith(route)) &&
+    !AUTH_ROUTES.some((route) => context.url.pathname.startsWith(route))
+  ) {
+    return next()
+  }
+
   const isAuthed = await auth.api.getSession({
     headers: context.request.headers,
   })
 
-  // Set user and session in locals
   context.locals.user = isAuthed ? isAuthed.user : null
   context.locals.session = isAuthed ? isAuthed.session : null
 
-  // Redirect to login if not authenticated and trying to access protected routes
-  if (
-    !isAuthed &&
-    PROTECTED_ROUTES.some((route) => context.url.pathname.startsWith(route))
-  ) {
+  // Accessing a protected route without being logged in
+  if (!isAuthed) {
     return new Response(null, {
       status: 302,
       headers: {
@@ -25,8 +33,8 @@ export const onRequest = defineMiddleware(async (context, next) => {
     })
   }
 
-  if (isAuthed && context.url.pathname.startsWith('/auth')) {
-    // Redirect to dashboard if authenticated and trying to access auth routes
+  // Accessing an auth route while logged in
+  if (context.url.pathname.startsWith('/auth')) {
     return new Response(null, {
       status: 302,
       headers: {
@@ -35,6 +43,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
     })
   }
 
-  // Otherwise, continue to the next middleware or route handler
+  // Everything is good, proceed to route
   return next()
 })
