6cf785411e355bc35d60ed1c9448f696158463d1
Keeghan McGarry <keeghan_m@live.com>
2025-09-01 12:59:47 +0100

    feat(editor): Added more buttons and functionality to the editor toolbar

--- src/components/app/components/slate-handling/editor/editor-toolbar.tsx
+++ src/components/app/components/slate-handling/editor/editor-toolbar.tsx
@@ -1,9 +1,17 @@
 import { Button } from '@components/ui/button'
 import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@components/ui/select'
+import {
   BoldIcon,
   ImageIcon,
   ItalicIcon,
   Link2Icon,
+  Table,
   UnderlineIcon,
 } from 'lucide-react'
 import { Transforms } from 'slate'
@@ -50,7 +58,6 @@ export default function EditorToolbar({
       >
         <BoldIcon className='h-4 w-4' />
       </Button>
-
       <Button
         variant='ghost'
         onMouseDown={(e) => {
@@ -60,7 +67,6 @@ export default function EditorToolbar({
       >
         <ItalicIcon className='h-4 w-5' />
       </Button>
-
       <Button
         variant='ghost'
         onMouseDown={(e) => {
@@ -69,6 +75,38 @@ export default function EditorToolbar({
         }}
       >
         <UnderlineIcon className='h-4 w-4' />
+      </Button>
+      <Select
+        onValueChange={(value) => {
+          Transforms.insertText(editor, `#`.repeat(parseInt(value[1])) + ' ')
+        }}
+      >
+        <SelectTrigger className='!text-foreground w-fit'>
+          <SelectValue placeholder='Title' />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value='h1'>Heading 1</SelectItem>
+          <SelectItem value='h2'>Heading 2</SelectItem>
+          <SelectItem value='h3'>Heading 3</SelectItem>
+          <SelectItem value='h4'>Heading 4</SelectItem>
+          <SelectItem value='h5'>Heading 5</SelectItem>
+        </SelectContent>
+      </Select>
+      <Button
+        variant='ghost'
+        onMouseDown={(e) => {
+          e.preventDefault()
+
+          Transforms.insertText(
+            editor,
+            `| Column 1 | Column 2 |
+| -------- | -------- |
+| Text     | Text     |
+| Text     | Text     |`
+          )
+        }}
+      >
+        <Table className='h-4 w-4' />
       </Button>
     </div>
   )
